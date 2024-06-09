bdb1c38e8023a1f127fe5e0a0f3904d5920d6e2b
gokulngk1 <gokulngk1@gmail.com>
2024-06-09 23:36:08 +0530

    navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import "./Home.css"
 import NavBar from '../NavBar/NavBar'
 
+// import "/Users/gokul/Code/reactjs/Divya/E-com/src/assets/image.jpg"
+
 const Home = () => {
   return (
     <div>
       <NavBar/>
+      <div className="img"></div>
+
+      <h1>Home</h1>
       
       </div>
   )
--- src/Pages/NavBar/NavBar.jsx
+++ src/Pages/NavBar/NavBar.jsx
@@ -1,17 +1,57 @@
+// import React from 'react'
+// import "./NavBar.css"
+// import { Link } from 'react-router-dom'
+
+// const NavBar = () => {
+//   return (
+//     <div className='header'>
+//         <Link to={"/"}><h1>wellcom</h1></Link>
+//         <ul className="nav-menu">
+//             <li>
+//                 <Link to={"/"}>Home</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/AboutMe"}>AboutMe</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Project"}>Project</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Career"}>Career</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Service"}>Service</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Contact"}>Contact</Link>
+//             </li>
+//         </ul>
+//     </div>
+//   )
+// }
+
+// export default NavBar
+
+
+
+// chat
+
 import React from 'react'
 import "./NavBar.css"
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div className='header'>
-        <Link to={"/"}><h1>wellcom</h1></Link>
-        <ul>
+    <nav className='nav-header'>
+        <div className="logo">
+            <Link to={"/"}><h1>Welcome</h1></Link>
+        </div>
+        <ul className="nav-menu">
             <li>
                 <Link to={"/"}>Home</Link>
             </li>
             <li>
-                <Link to={"/AboutMe"}>AboutMe</Link>
+                <Link to={"/AboutMe"}>About Me</Link>
             </li>
             <li>
                 <Link to={"/Project"}>Project</Link>
@@ -26,7 +66,7 @@ const NavBar = () => {
                 <Link to={"/Contact"}>Contact</Link>
             </li>
         </ul>
-    </div>
+    </nav>
   )
 }
 
