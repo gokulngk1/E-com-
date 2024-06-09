69c039a4be083ee877283635cdfdb8ae3fe89b8e
gokulngk1 <gokulngk1@gmail.com>
2024-06-09 23:41:24 +0530

    agin back

--- src/Pages/NavBar/NavBar.jsx
+++ src/Pages/NavBar/NavBar.jsx
@@ -1,57 +1,17 @@
-// import React from 'react'
-// import "./NavBar.css"
-// import { Link } from 'react-router-dom'
-
-// const NavBar = () => {
-//   return (
-//     <div className='header'>
-//         <Link to={"/"}><h1>wellcom</h1></Link>
-//         <ul className="nav-menu">
-//             <li>
-//                 <Link to={"/"}>Home</Link>
-//             </li>
-//             <li>
-//                 <Link to={"/AboutMe"}>AboutMe</Link>
-//             </li>
-//             <li>
-//                 <Link to={"/Project"}>Project</Link>
-//             </li>
-//             <li>
-//                 <Link to={"/Career"}>Career</Link>
-//             </li>
-//             <li>
-//                 <Link to={"/Service"}>Service</Link>
-//             </li>
-//             <li>
-//                 <Link to={"/Contact"}>Contact</Link>
-//             </li>
-//         </ul>
-//     </div>
-//   )
-// }
-
-// export default NavBar
-
-
-
-// chat
-
 import React from 'react'
 import "./NavBar.css"
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <nav className='nav-header'>
-        <div className="logo">
-            <Link to={"/"}><h1>Welcome</h1></Link>
-        </div>
+    <div className='header'>
+        <Link to={"/"}><h1>wellcom</h1></Link>
         <ul className="nav-menu">
             <li>
                 <Link to={"/"}>Home</Link>
             </li>
             <li>
-                <Link to={"/AboutMe"}>About Me</Link>
+                <Link to={"/AboutMe"}>AboutMe</Link>
             </li>
             <li>
                 <Link to={"/Project"}>Project</Link>
@@ -66,8 +26,48 @@ const NavBar = () => {
                 <Link to={"/Contact"}>Contact</Link>
             </li>
         </ul>
-    </nav>
+    </div>
   )
 }
 
 export default NavBar
+
+
+
+// chat
+
+// import React from 'react'
+// import "./NavBar.css"
+// import { Link } from 'react-router-dom'
+
+// const NavBar = () => {
+//   return (
+//     <nav className='nav-header'>
+//         <div className="logo">
+//             <Link to={"/"}><h1>Welcome</h1></Link>
+//         </div>
+//         <ul className="nav-menu">
+//             <li>
+//                 <Link to={"/"}>Home</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/AboutMe"}>About Me</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Project"}>Project</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Career"}>Career</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Service"}>Service</Link>
+//             </li>
+//             <li>
+//                 <Link to={"/Contact"}>Contact</Link>
+//             </li>
+//         </ul>
+//     </nav>
+//   )
+// }
+
+// export default NavBar
